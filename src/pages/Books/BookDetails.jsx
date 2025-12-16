6c2a55b8c1b183ac7685776d40c98beb5c85b5c8
junayed17 <junayedahmednipun@gmail.com>
2025-12-16 16:21:01 +0600

    connect book order form with backend API

--- src/pages/Books/BookDetails.jsx
+++ src/pages/Books/BookDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import img1 from "../../assets/0bfad7aa-9e1b-4802-b958-879cefd2efe1.jfif";
 import img2 from "../../assets/8841be02-d0ea-4f43-a1a5-c58a4d00d695.jfif";
 import {
@@ -15,11 +15,20 @@ import useAxiosSecure from "../../customHook/useAxiosSecure";
 import Loader from "../../components/Loader";
 import useAuthHook from "../../customHook/useAuthHook";
 import toast from "react-hot-toast";
+import { MdCancel } from "react-icons/md";
+import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
 
 const BookDetails = () => {
   const axiosSecure = useAxiosSecure();
   const { id } = useParams();
   const { user } = useAuthHook();
+  const modalRef = useRef();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   const { data } = useQuery({
     queryKey: ["book", id],
     queryFn: async () => {
@@ -46,14 +55,53 @@ const BookDetails = () => {
       author: data.author,
       addedAt: new Date(),
     };
+    axiosSecure.post("/wishListBook", wishData).then((result) => {
+      if (result.data.insertedId) {
+        toast.success("Added to wish list sucessfully");
+      }
+    });
+  }
+  function handleShowModal() {
+    modalRef.current.showModal();
+  }
+
+  function handleBookOrder(UserData) {
+    UserData.sellerEmail = data.ownerEmail;
+    UserData.bookId = data._id;
+    UserData.orderAt = new Date();
+    UserData.status = "pending";
+    UserData.payment = "unpaid";
+
     axiosSecure
-      .post("/wishListBook", wishData)
-      .then((result) =>{ 
+      .post("/orders", UserData)
+      .then((result) => {
         if (result.data.insertedId) {
-          toast.success("Added to wish list sucessfully");
+          modalRef.current.close();
+          Swal.fire({
+            title: "order sucessfully done",
+            icon: "success",
+            draggable: true,
+          });
         }
+      })
+      .catch((err) => {
+        Swal.fire({
+          title: "Somthing Error!",
+          icon: "error",
+          draggable: true,
+        });
       });
   }
+
+  function handleCancel() {
+    modalRef.current.close();
+    Swal.fire({
+      title: "not ordered",
+      icon: "error",
+      draggable: true,
+    });
+  }
+
   return (
     <div className="bg-gradient-to-br from-base-100 via-base-200 to-base-300 min-h-screen py-14 rounded-2xl my-10">
       <div className="max-w-7xl mx-auto px-4">
@@ -141,9 +189,11 @@ const BookDetails = () => {
 
             {/* CTA */}
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 pt-6 items-center">
-              <Link
+              <button
                 class="relative inline-flex items-center justify-center px-8 py-4 overflow-hidden tracking-tighter text-white bg-gray-800 rounded-md group my-4 mx-4"
                 type="button"
+                // onClick={handleBookOrder}
+                onClick={handleShowModal}
               >
                 <span class="absolute w-0 h-0 transition-all duration-500 ease-out bg-orange-600 rounded-full group-hover:w-full group-hover:h-56"></span>
                 <span class="absolute bottom-0 left-0 h-full -ml-2">
@@ -176,9 +226,9 @@ const BookDetails = () => {
                 </span>
                 <span class="absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent to-gray-200"></span>
                 <span class="relative text-sm md:text-lg font-bold heading">
-                  Buy Now
+                  Order now
                 </span>
-              </Link>
+              </button>
 
               <button
                 className="
@@ -203,6 +253,191 @@ const BookDetails = () => {
             </div>
           </div>
         </div>
+        <dialog id="my_modal_3" className="modal" ref={modalRef}>
+          <div className="modal-box">
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn btn-xl text-4xl btn-circle btn-ghost absolute right-2 top-2">
+                <MdCancel />
+              </button>
+            </form>
+            <form
+              action=""
+              className="w-full max-w-lg shadow-2xl bg-base-100 px-8 py-8 rounded-2xl"
+              onSubmit={handleSubmit(handleBookOrder)}
+            >
+              <h2 className="text-3xl md:text-5xl font-bold mb-4 leading-tight heading font-bold text-center">
+                Welcome Back
+              </h2>
+              <p className="text-lg md:text-2xl text-center bodyFont">
+                Update with BoolQurier
+              </p>
+              <fieldset className="fieldset">
+                <div className="my-2">
+                  <label
+                    class="block text-blue-400 text-sm md:text-lg font-medium mb-2 bodyFont"
+                    for="name"
+                  >
+                    Your Name
+                  </label>
+                  <input
+                    class="text-sm custom-input w-full px-4 py-4 border border-gray-300 rounded-lg shadow-sm transition duration-300 ease-in-out transform focus:-translate-y-1 focus:outline-blue-300 hover:shadow-lg hover:border-blue-300 "
+                    placeholder="Md.Example"
+                    type="text"
+                    id="name"
+                    {...register("name", { required: "name is required" })}
+                    defaultValue={user.displayName}
+                    readOnly
+                  />
+                  {errors.name && (
+                    <p className="text-red-500 text-sm">
+                      {errors.name.message}
+                    </p>
+                  )}
+                </div>
+
+                <div className="my-2">
+                  <label
+                    class="block text-blue-400 text-sm md:text-lg font-medium mb-2 bodyFont"
+                    for="email"
+                  >
+                    Your email
+                  </label>
+                  <input
+                    class="text-sm custom-input w-full px-4 py-4 border border-gray-300 rounded-lg shadow-sm transition duration-300 ease-in-out transform focus:-translate-y-1 focus:outline-blue-300 hover:shadow-lg hover:border-blue-300 "
+                    placeholder="Example@gmail.com"
+                    type="email"
+                    id="email"
+                    {...register("email")}
+                    defaultValue={user.email}
+                    readOnly
+                  />
+                </div>
+                <div className="my-2">
+                  <label
+                    class="block text-blue-400 text-sm md:text-lg font-medium mb-2 bodyFont"
+                    for="contact"
+                  >
+                    Contact
+                  </label>
+                  <input
+                    className="text-sm custom-input w-full px-4 py-4 border border-gray-300 rounded-lg shadow-sm transition duration-300 ease-in-out transform focus:-translate-y-1 focus:outline-blue-300 hover:shadow-lg hover:border-blue-300"
+                    placeholder="01XXXXXXXXX"
+                    type="tel"
+                    {...register("number", {
+                      required: "Phone number is required",
+                      pattern: {
+                        value: /^01[3-9][0-9]{8}$/,
+                        message: "Invalid Bangladeshi phone number",
+                      },
+                    })}
+                  />
+                  {errors.number && (
+                    <p className="text-red-500 text-sm">
+                      {errors.number.message}
+                    </p>
+                  )}
+                </div>
+                <div className="my-2">
+                  <label
+                    class="block text-blue-400 text-sm md:text-lg font-medium mb-2 bodyFont"
+                    for="adress"
+                  >
+                    Adress
+                  </label>
+                  <input
+                    class="text-sm custom-input w-full px-4 py-4 border border-gray-300 rounded-lg shadow-sm transition duration-300 ease-in-out transform focus:-translate-y-1 focus:outline-blue-300 hover:shadow-lg hover:border-blue-300 "
+                    placeholder="dhaka,bng"
+                    type="text"
+                    id="adress"
+                    {...register("adress", { required: "adress is required" })}
+                  />
+                </div>
+
+                <div className="flex items-center justify-center gap-4">
+                  <button
+                    class="relative inline-flex items-center justify-center px-8 py-2.5 overflow-hidden tracking-tighter text-white bg-gray-800 rounded-md group my-4 w-full"
+                    type="submit"
+                  >
+                    <span class="absolute w-0 h-0 transition-all duration-500 ease-out bg-orange-600 rounded-full group-hover:w-full group-hover:h-56"></span>
+                    <span class="absolute bottom-0 left-0 h-full -ml-2">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="w-auto h-full opacity-100 object-stretch"
+                        viewBox="0 0 487 487"
+                      >
+                        <path
+                          fill-opacity=".1"
+                          fill-rule="nonzero"
+                          fill="#FFF"
+                          d="M0 .3c67 2.1 134.1 4.3 186.3 37 52.2 32.7 89.6 95.8 112.8 150.6 23.2 54.8 32.3 101.4 61.2 149.9 28.9 48.4 77.7 98.8 126.4 149.2H0V.3z"
+                        ></path>
+                      </svg>
+                    </span>
+                    <span class="absolute top-0 right-0 w-12 h-full -mr-3">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="object-cover w-full h-full"
+                        viewBox="0 0 487 487"
+                      >
+                        <path
+                          fill-opacity=".1"
+                          fill-rule="nonzero"
+                          fill="#FFF"
+                          d="M487 486.7c-66.1-3.6-132.3-7.3-186.3-37s-95.9-85.3-126.2-137.2c-30.4-51.8-49.3-99.9-76.5-151.4C70.9 109.6 35.6 54.8.3 0H487v486.7z"
+                        ></path>
+                      </svg>
+                    </span>
+                    <span class="absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent to-gray-200"></span>
+                    <span class="relative text-sm md:text-lg font-bold heading">
+                      Order Now
+                    </span>
+                  </button>
+                  <button
+                    class="relative inline-flex items-center justify-center px-8 py-2.5 overflow-hidden tracking-tighter text-white bg-gray-800 rounded-md group my-4 btn-circle w-full"
+                    id="cancel"
+                    onClick={handleCancel}
+                    type="button"
+                  >
+                    <span class="absolute w-0 h-0 transition-all duration-500 ease-out bg-orange-600 rounded-full group-hover:w-full group-hover:h-56"></span>
+                    <span class="absolute bottom-0 left-0 h-full -ml-2">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="w-auto h-full opacity-100 object-stretch"
+                        viewBox="0 0 487 487"
+                      >
+                        <path
+                          fill-opacity=".1"
+                          fill-rule="nonzero"
+                          fill="#FFF"
+                          d="M0 .3c67 2.1 134.1 4.3 186.3 37 52.2 32.7 89.6 95.8 112.8 150.6 23.2 54.8 32.3 101.4 61.2 149.9 28.9 48.4 77.7 98.8 126.4 149.2H0V.3z"
+                        ></path>
+                      </svg>
+                    </span>
+                    <span class="absolute top-0 right-0 w-12 h-full -mr-3">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="object-cover w-full h-full"
+                        viewBox="0 0 487 487"
+                      >
+                        <path
+                          fill-opacity=".1"
+                          fill-rule="nonzero"
+                          fill="#FFF"
+                          d="M487 486.7c-66.1-3.6-132.3-7.3-186.3-37s-95.9-85.3-126.2-137.2c-30.4-51.8-49.3-99.9-76.5-151.4C70.9 109.6 35.6 54.8.3 0H487v486.7z"
+                        ></path>
+                      </svg>
+                    </span>
+                    <span class="absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent to-gray-200"></span>
+                    <span class="relative text-sm md:text-lg font-bold heading">
+                      Cancel
+                    </span>
+                  </button>
+                </div>
+              </fieldset>
+            </form>
+          </div>
+        </dialog>
       </div>
     </div>
   );
