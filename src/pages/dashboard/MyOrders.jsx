d2396f7d02741c2aa15ad81290f788dc20badc98
junayed17 <junayedahmednipun@gmail.com>
2025-12-20 17:48:23 +0600

    order calcel based on conditionally rendered functionality added

--- src/pages/dashboard/MyOrders.jsx
+++ src/pages/dashboard/MyOrders.jsx
@@ -17,14 +17,11 @@ const MyOrders = () => {
       return result.data;
     },
   });
-
-  console.log(data, error, isLoading);
-
   async function deleteOrder(id) {
     const result = await axiosSecure.delete(`/order/delete/${id}`);
-    console.log(result.data);
     refetch();
   }
+
 
   if (isLoading) {
     return <Loader />;
@@ -80,51 +77,53 @@ const MyOrders = () => {
                 </span>
               </td>
 
-              {book.payment === "paid" || book.status !== "Pending" ? (
-                <td className="text-center flex items-center justify-center gap-2 text-green-600 bodyFont font bold">
-                  paid
-                </td>
-              ) : (
+             
+            
                 <td className="text-center flex items-center justify-center gap-2">
-                  <Link
-                    class="relative inline-flex items-center justify-center w-22 px-4 py-2 overflow-hidden tracking-tighter text-white bg-gray-800 rounded-md group my-4"
-                    to={`/dashboard/payment/${book.bookId}`}
-                  >
-                    <span class="absolute w-0 h-0 transition-all duration-500 ease-out bg-orange-600 rounded-full group-hover:w-full group-hover:h-56"></span>
-                    <span class="absolute bottom-0 left-0 h-full -ml-2">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="w-auto h-full opacity-100 object-stretch"
-                        viewBox="0 0 487 487"
-                      >
-                        <path
-                          fill-opacity=".1"
-                          fill-rule="nonzero"
-                          fill="#FFF"
-                          d="M0 .3c67 2.1 134.1 4.3 186.3 37 52.2 32.7 89.6 95.8 112.8 150.6 23.2 54.8 32.3 101.4 61.2 149.9 28.9 48.4 77.7 98.8 126.4 149.2H0V.3z"
-                        ></path>
-                      </svg>
-                    </span>
-                    <span class="absolute top-0 right-0 w-12 h-full -mr-3">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="object-cover w-full h-full"
-                        viewBox="0 0 487 487"
-                      >
-                        <path
-                          fill-opacity=".1"
-                          fill-rule="nonzero"
-                          fill="#FFF"
-                          d="M487 486.7c-66.1-3.6-132.3-7.3-186.3-37s-95.9-85.3-126.2-137.2c-30.4-51.8-49.3-99.9-76.5-151.4C70.9 109.6 35.6 54.8.3 0H487v486.7z"
-                        ></path>
-                      </svg>
-                    </span>
-                    <span class="absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent to-gray-200"></span>
-                    <span class="relative text-sm  font-bold heading">
-                      Pay Now
-                    </span>
-                  </Link>
-                  <button
+                  {book.payment === "paid" ? (
+                    " paid"
+                  ) : (
+                    <Link
+                      class="relative inline-flex items-center justify-center w-22 px-4 py-2 overflow-hidden tracking-tighter text-white bg-gray-800 rounded-md group my-4"
+                      to={`/dashboard/payment/${book.bookId}`}
+                    >
+                      <span class="absolute w-0 h-0 transition-all duration-500 ease-out bg-orange-600 rounded-full group-hover:w-full group-hover:h-56"></span>
+                      <span class="absolute bottom-0 left-0 h-full -ml-2">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          class="w-auto h-full opacity-100 object-stretch"
+                          viewBox="0 0 487 487"
+                        >
+                          <path
+                            fill-opacity=".1"
+                            fill-rule="nonzero"
+                            fill="#FFF"
+                            d="M0 .3c67 2.1 134.1 4.3 186.3 37 52.2 32.7 89.6 95.8 112.8 150.6 23.2 54.8 32.3 101.4 61.2 149.9 28.9 48.4 77.7 98.8 126.4 149.2H0V.3z"
+                          ></path>
+                        </svg>
+                      </span>
+                      <span class="absolute top-0 right-0 w-12 h-full -mr-3">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          class="object-cover w-full h-full"
+                          viewBox="0 0 487 487"
+                        >
+                          <path
+                            fill-opacity=".1"
+                            fill-rule="nonzero"
+                            fill="#FFF"
+                            d="M487 486.7c-66.1-3.6-132.3-7.3-186.3-37s-95.9-85.3-126.2-137.2c-30.4-51.8-49.3-99.9-76.5-151.4C70.9 109.6 35.6 54.8.3 0H487v486.7z"
+                          ></path>
+                        </svg>
+                      </span>
+                      <span class="absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent to-gray-200"></span>
+                      <span class="relative text-sm  font-bold heading">
+                        Pay Now
+                      </span>
+                    </Link>
+                  )}
+
+ {book.status === "Pending" ?  <button
                     class="relative inline-flex items-center justify-center px-4 py-2 overflow-hidden tracking-tighter text-white bg-gray-800 rounded-md group my-4"
                     type="button"
                     onClick={() => deleteOrder(book.bookId)}
@@ -162,9 +161,9 @@ const MyOrders = () => {
                     <span class="relative text-sm  font-bold heading">
                       Cancel
                     </span>
-                  </button>
+                  </button>:""}
+                 
                 </td>
-              )}
             </tr>
           ))}
         </tbody>
